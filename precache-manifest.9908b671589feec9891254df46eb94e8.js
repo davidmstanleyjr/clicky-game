fd1e56617ec6a68e5709f83f680071a692c6b085
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2020-01-04 12:26:54 -0500

    Updates

---
+++ precache-manifest.9908b671589feec9891254df46eb94e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2129e4266d3c82357fe80ab0edebd12a",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "7a0ef6fd908262efd20b",
+    "url": "/clicky-game/static/css/main.cdbe7584.chunk.css"
+  },
+  {
+    "revision": "dde8e28cad0c1325beb0",
+    "url": "/clicky-game/static/js/2.b71f355c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.b71f355c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a0ef6fd908262efd20b",
+    "url": "/clicky-game/static/js/main.e0f04d86.chunk.js"
+  },
+  {
+    "revision": "b5e3d5480cb174bedaf8",
+    "url": "/clicky-game/static/js/runtime-main.dbb64346.js"
+  }
+]);
